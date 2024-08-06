cdfaac517ceecf3ae15d5d8736fb23b703ec1e5f
James O'Kane <orbiccode@gmail.com>
2024-08-06 13:36:56 +0100

    1st test function

--- scrabble-score.test.js
+++ scrabble-score.test.js
@@ -1,1 +1,11 @@
 import { calculateScrabbleScore } from "./scrabble-score";
+
+// Write a new test that checks whether `calculateScrabbleScore` correctly calculates the score for the word `A`.
+
+
+
+test( 'chack if correctly calculates the scroe of A', () => {
+    const result = calculateScrabbleScore('A');
+    expect(result).toBe(1);
+    return result
+})

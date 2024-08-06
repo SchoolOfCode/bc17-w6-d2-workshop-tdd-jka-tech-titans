9cf1d7350ef11c069fe076311a199adacd40eb48
Aidan Isbell <IOLV@Outlook.com>
2024-08-06 14:25:18 +0100

    Ticket 2 started

--- scrabble-score.js
+++ scrabble-score.js
@@ -1,3 +1,32 @@
 export function calculateScrabbleScore(word) {
-    
+	const point1 = ["A", "E", "I", "L", "N", "O", "R", "S", "T", "U"];
+	const point2 = ["D", "G"];
+	const point3 = ["B", "C", "M", "P"];
+	const point4 = ["F", "H", "V", "W", "Y"];
+	const point5 = ["K"];
+	const point6 = ["J", "X"];
+	const point7 = ["Q", "Z"];
+	const points = [point1, point2, point3, point4, point5, point6, point7];
+	let pointsIndex = 0;
+
+	for (let i = 0; i <= points[pointsIndex].length; i++) {
+		console.log(points[pointsIndex][i]);
+		if (word === points[pointsIndex][i]) {
+			return 1;
+		}
+	}
+
+	// if (word === "A") {
+	// 	return 1;
+	// } else if (word === "B") {
+	// 	return 3;
+	// }
 }
+
+// 1 = A, E, I, L, N, O, R, S, T, U
+// 2 = D, G
+// 3 = B, C, M, P
+// 4 = F, H, V, W, Y
+// 5 = K
+// 8 = J, X
+// 10 = Q, Z
--- scrabble-score.test.js
+++ scrabble-score.test.js
@@ -1,11 +1,17 @@
+import { test, expect } from "vitest";
 import { calculateScrabbleScore } from "./scrabble-score";
 
 // Write a new test that checks whether `calculateScrabbleScore` correctly calculates the score for the word `A`.
 
+test("check if correctly calculates the score of A", () => {
+	const result = calculateScrabbleScore("A");
+	expect(result).toBe(1);
+	console.log(`results = ${result}`);
+	return result;
+});
 
-
-test( 'chack if correctly calculates the scroe of A', () => {
-    const result = calculateScrabbleScore('A');
-    expect(result).toBe(1);
-    return result
-})
+// test("check if correctly calculates the score of B", () => {
+// 	const result = calculateScrabbleScore("B");
+// 	expect(result).toBe(3);
+// 	return result;
+// });
